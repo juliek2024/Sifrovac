9ff8402f3e3d6e68db12057e81511245cd138321
juliek2024 <julie.koubova.pm@gmail.com>
2025-02-20 21:22:17 +0100

    Update Encryption1.java

--- src/Encryption1.java
+++ src/Encryption1.java
@@ -2,20 +2,21 @@ import java.awt.*;
 
 import static java.awt.SystemColor.text;
 
-public class Encription1 {
+public class Encryption1 {
 
-    public static String reversedEncription (String text) {
-    String [] sentence = text.split(" ");
-    StringBuilder encripted1 = new StringBuilder();
+    public static String reversedEncryption (String text) {
+        String [] sentence = text.split(" ");
+        StringBuilder encrypted1 = new StringBuilder();
 
-    for (String word : sentence) {
-        String reversedWord = new StringBuilder(word).reverse().toString();
-        encripted1.append(reversedWord).append(" ");
+        for (String word : sentence) {
+            String cleanWord = word.replaceAll("[^a-zA-Z]", "");
+            String punctuation = word.replaceAll("[a-zA-Z]", "");
+            encrypted1.append(new StringBuilder(cleanWord).reverse()).append(punctuation).append(" ");
+        }
+        return encrypted1.toString().trim();
     }
-        return encripted1.toString().trim();
-    }
 
-    public static String reversedDecription (String text) {
-        return reversedEncription(text);
+    public static String reversedDecryption (String text) {
+        return reversedEncryption(text);
     }
 }
